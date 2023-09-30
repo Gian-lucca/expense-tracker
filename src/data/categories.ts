d72cdd28827de230de0486d3cecb0c261a02116d
Gianlucca Augusto <gianlucca.augusto@gmail.com>
2023-09-29 23:27:10 -0300

    Ajustes

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,7 @@
 import { Category } from '../types/Category';
 
 export const categories: Category = {
-    food: { title: 'Alimentação', color: 'blue', expense: true},
-    rent: { title: 'Aluguel', color: 'brown', expense:true},
+    food: { title: 'Cartão', color: 'blue', expense: true},
+    rent: { title: 'Contas', color: 'brown', expense:true},
     salary: { title: 'Salário', color: 'green', expense:false}
 }
